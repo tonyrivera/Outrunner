dec6d042e708872ce174995bd0d88564c4172a97
Tony Rivera <tonyrivera@users.noreply.github.com>
2018-04-23 15:24:44 -0700

    Initial Commit

---
+++ Outrunner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Outrunner
+//
+//  Created by Tony on 4/23/18.
+//  Copyright © 2018 Tony Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
